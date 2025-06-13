2bf4e5a9bf7744b0da304e2d9f1c15ca95800299
Anaru Herbert <anaruherbert@gmail.com>
2025-06-13 20:00:14 +1000

    fix auth

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -6,7 +6,9 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      afterSignOutUrl={process.env.NEXT_PUBLIC_REDIRECT_AFTER_SIGNOUT_URL}
+    >
       <ConvexClientProvider>
         <TopNav />
         {children}
