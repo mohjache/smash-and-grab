9ed8c1ed5003050e651c78417a78ca2525432fa1
Anaru Herbert <anaruherbert@gmail.com>
2025-06-13 20:05:31 +1000

    fix header

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -9,7 +9,7 @@ export const TopNav = () => {
     <div className="bg-background text-primary fixed top-0 right-0 left-0 z-50 flex h-16 justify-between px-4 lg:px-32">
       <div className="mr-4 flex">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <span className="font-bold">upCoach</span>
+          <span className="font-bold">Smash&apos;n Grab</span>
         </Link>
       </div>
       <div className="flex items-center justify-center">
